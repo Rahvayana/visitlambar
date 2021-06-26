52345c5b0776c099325928eed47e8bcef4323789
rahvayana <fahrursanjaya@gmail.com>
2021-06-26 18:11:21 +0700

    controller web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\cms\CmsController;
+use App\Http\Controllers\cms\TourController;
+use App\Http\Controllers\cms\DestinationController;
+use App\Http\Controllers\cms\SettingController;
+use App\Http\Controllers\cms\TeamController;
+use App\Http\Controllers\cms\ServiceController;
+use App\Http\Controllers\Cms\AvailableController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\CustomersController;
 /*
@@ -48,55 +55,55 @@ Route::get('/tourpack/payment', [BookingController::class, 'return'])->name('frn
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\cms\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 // Route::get('/admin/dashboard', [CmsController::class, 'dashboard'])->name('cms.dashboard')->middleware('auth');
-Route::get('/admin/dashboard', [App\Http\Controllers\cms\CmsController::class, 'dashboard'])->name('cms.dashboard');
+Route::get('/admin/dashboard', [CmsController::class, 'dashboard'])->name('cms.dashboard');
 
 
 //tour
-Route::get('/admin/tour', [App\Http\Controllers\cms\TourController::class, 'index'])->name('cms.tour.index');
-Route::get('/admin/tour/add', [App\Http\Controllers\cms\TourController::class, 'create'])->name('cms.tour.add');
-Route::post('/admin/tour/store', [App\Http\Controllers\cms\TourController::class, 'store'])->name('cms.tour.store');
-Route::delete('/admin/tour/destroy/{id}', [App\Http\Controllers\cms\TourController::class, 'destroy'])->name('cms.tour.destroy');
-Route::get('/admin/tour/edit/{id}', [App\Http\Controllers\cms\TourController::class, 'edit'])->name('cms.tour.edit');
-Route::post('/admin/tour/update/{id}', [App\Http\Controllers\cms\TourController::class, 'update'])->name('cms.tour.update');
+Route::get('/admin/tour', [TourController::class, 'index'])->name('cms.tour.index');
+Route::get('/admin/tour/add', [TourController::class, 'create'])->name('cms.tour.add');
+Route::post('/admin/tour/store', [TourController::class, 'store'])->name('cms.tour.store');
+Route::delete('/admin/tour/destroy/{id}', [TourController::class, 'destroy'])->name('cms.tour.destroy');
+Route::get('/admin/tour/edit/{id}', [TourController::class, 'edit'])->name('cms.tour.edit');
+Route::post('/admin/tour/update/{id}', [TourController::class, 'update'])->name('cms.tour.update');
 
 //destination
-Route::get('/admin/destination', [App\Http\Controllers\cms\DestinationController::class, 'index'])->name('cms.destination.index');
-Route::get('/admin/destination/add', [App\Http\Controllers\cms\DestinationController::class, 'create'])->name('cms.destination.add');
-Route::get('/admin/destination/edit/{id}', [App\Http\Controllers\cms\DestinationController::class, 'edit'])->name('cms.destination.edit');
-Route::post('/admin/destination/store', [App\Http\Controllers\cms\DestinationController::class, 'store'])->name('cms.destination.store');
-Route::delete('/admin/destination/destroy/{id}', [App\Http\Controllers\cms\DestinationController::class, 'destroy'])->name('cms.destination.destroy');
-Route::post('/admin/destination/update{id}', [App\Http\Controllers\cms\DestinationController::class, 'update'])->name('cms.destination.update');
+Route::get('/admin/destination', [DestinationController::class, 'index'])->name('cms.destination.index');
+Route::get('/admin/destination/add', [DestinationController::class, 'create'])->name('cms.destination.add');
+Route::get('/admin/destination/edit/{id}', [DestinationController::class, 'edit'])->name('cms.destination.edit');
+Route::post('/admin/destination/store', [DestinationController::class, 'store'])->name('cms.destination.store');
+Route::delete('/admin/destination/destroy/{id}', [DestinationController::class, 'destroy'])->name('cms.destination.destroy');
+Route::post('/admin/destination/update{id}', [DestinationController::class, 'update'])->name('cms.destination.update');
 
 //setting
-Route::get('/admin/setting/contact-us', [App\Http\Controllers\cms\SettingController::class, 'contactShow'])->name('cms.setting.contact');
-Route::post('/admin/setting/contact-us/update', [App\Http\Controllers\cms\SettingController::class, 'contactUpdate'])->name('cms.setting.contact.update');
-Route::get('/admin/setting/about-us', [App\Http\Controllers\cms\SettingController::class, 'aboutShow'])->name('cms.setting.about');
-Route::post('/admin/setting/about-us/update', [App\Http\Controllers\cms\SettingController::class, 'aboutFeatureUpdate'])->name('cms.setting.about.update');
-Route::get('/admin/setting/partner', [App\Http\Controllers\cms\SettingController::class, 'partner'])->name('cms.setting.partner');
-Route::post('/admin/setting/partner/destroy/{id}', [App\Http\Controllers\cms\SettingController::class, 'partnerDestroy'])->name('cms.setting.partner.destroy');
-Route::post('/admin/setting/partner/store', [App\Http\Controllers\cms\SettingController::class, 'partnerStore'])->name('cms.setting.partner.store');
+Route::get('/admin/setting/contact-us', [SettingController::class, 'contactShow'])->name('cms.setting.contact');
+Route::post('/admin/setting/contact-us/update', [SettingController::class, 'contactUpdate'])->name('cms.setting.contact.update');
+Route::get('/admin/setting/about-us', [SettingController::class, 'aboutShow'])->name('cms.setting.about');
+Route::post('/admin/setting/about-us/update', [SettingController::class, 'aboutFeatureUpdate'])->name('cms.setting.about.update');
+Route::get('/admin/setting/partner', [SettingController::class, 'partner'])->name('cms.setting.partner');
+Route::post('/admin/setting/partner/destroy/{id}', [SettingController::class, 'partnerDestroy'])->name('cms.setting.partner.destroy');
+Route::post('/admin/setting/partner/store', [SettingController::class, 'partnerStore'])->name('cms.setting.partner.store');
 
 //team
-Route::get('/admin/setting/team', [App\Http\Controllers\cms\TeamController::class, 'index'])->name('cms.setting.team');
-Route::get('/admin/setting/team/add', [App\Http\Controllers\cms\TeamController::class, 'create'])->name('cms.setting.team.add');
-Route::post('/admin/setting/team/store', [App\Http\Controllers\cms\TeamController::class, 'store'])->name('cms.setting.team.store');
-Route::delete('/admin/setting/team/destroy/{id}', [App\Http\Controllers\cms\TeamController::class, 'destroy'])->name('cms.setting.team.destroy');
-Route::get('/admin/setting/team/edit/{id}', [App\Http\Controllers\cms\TeamController::class, 'edit'])->name('cms.setting.team.edit');
-Route::post('/admin/setting/team/update/{id}', [App\Http\Controllers\cms\TeamController::class, 'update'])->name('cms.setting.team.update');
+Route::get('/admin/setting/team', [TeamController::class, 'index'])->name('cms.setting.team');
+Route::get('/admin/setting/team/add', [TeamController::class, 'create'])->name('cms.setting.team.add');
+Route::post('/admin/setting/team/store', [TeamController::class, 'store'])->name('cms.setting.team.store');
+Route::delete('/admin/setting/team/destroy/{id}', [TeamController::class, 'destroy'])->name('cms.setting.team.destroy');
+Route::get('/admin/setting/team/edit/{id}', [TeamController::class, 'edit'])->name('cms.setting.team.edit');
+Route::post('/admin/setting/team/update/{id}', [TeamController::class, 'update'])->name('cms.setting.team.update');
 
 //service
-Route::get('/admin/setting/service', [App\Http\Controllers\cms\ServiceController::class, 'index'])->name('cms.setting.service');
-Route::get('/admin/setting/service/add', [App\Http\Controllers\cms\ServiceController::class, 'create'])->name('cms.setting.service.add');
-Route::post('/admin/setting/service/store', [App\Http\Controllers\cms\ServiceController::class, 'store'])->name('cms.setting.service.store');
-Route::get('/admin/setting/service/edit/{id}', [App\Http\Controllers\cms\ServiceController::class, 'edit'])->name('cms.setting.service.edit');
-Route::post('/admin/setting/service/update/{id}', [App\Http\Controllers\cms\ServiceController::class, 'update'])->name('cms.setting.service.update');
-Route::delete('/admin/setting/service/destroy/{id}', [App\Http\Controllers\cms\ServiceController::class, 'destroy'])->name('cms.setting.service.destroy');
+Route::get('/admin/setting/service', [ServiceController::class, 'index'])->name('cms.setting.service');
+Route::get('/admin/setting/service/add', [ServiceController::class, 'create'])->name('cms.setting.service.add');
+Route::post('/admin/setting/service/store', [ServiceController::class, 'store'])->name('cms.setting.service.store');
+Route::get('/admin/setting/service/edit/{id}', [ServiceController::class, 'edit'])->name('cms.setting.service.edit');
+Route::post('/admin/setting/service/update/{id}', [ServiceController::class, 'update'])->name('cms.setting.service.update');
+Route::delete('/admin/setting/service/destroy/{id}', [ServiceController::class, 'destroy'])->name('cms.setting.service.destroy');
 
-Route::get('/admin/available', [App\Http\Controllers\cms\AvailableController::class, 'index'])->name('cms.available.index');
-Route::get('/admin/available/add', [App\Http\Controllers\cms\AvailableController::class, 'create'])->name('cms.available.create');
-Route::post('/admin/available/store', [App\Http\Controllers\cms\AvailableController::class, 'store'])->name('cms.available.store');
-Route::delete('/admin/available/destroy/{id}', [App\Http\Controllers\cms\AvailableController::class, 'destroy'])->name('cms.available.destroy');
-Route::get('/admin/available/edit/{id}', [App\Http\Controllers\cms\AvailableController::class, 'edit'])->name('cms.available.edit');
-Route::post('/admin/available/update/{id}', [App\Http\Controllers\cms\AvailableController::class, 'update'])->name('cms.available.update');
+Route::get('/admin/available', [AvailableController::class, 'index'])->name('cms.available.index');
+Route::get('/admin/available/add', [AvailableController::class, 'create'])->name('cms.available.create');
+Route::post('/admin/available/store', [AvailableController::class, 'store'])->name('cms.available.store');
+Route::delete('/admin/available/destroy/{id}', [AvailableController::class, 'destroy'])->name('cms.available.destroy');
+Route::get('/admin/available/edit/{id}', [AvailableController::class, 'edit'])->name('cms.available.edit');
+Route::post('/admin/available/update/{id}', [AvailableController::class, 'update'])->name('cms.available.update');
